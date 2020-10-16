50d95b3a27fe12c3cbd8b8c9b6d9ddd20b7a25cf
dixitabhishek239 <dixitabhishek239@icloud.com>
2020-10-16 12:20:58 +0530

    Login Page Controller and fxml files

---
+++ DBMS_Mini_LibraryManagementSystem/src/libraryManagementSystem/controllers/LoginPageController.java
@@ -0,0 +1,45 @@
+package libraryManagementSystem.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginPageController {
+
+    @FXML
+    private ChoiceBox<?> loginDropDown;
+
+    @FXML
+    private TextField usernameId;
+
+    @FXML
+    private PasswordField passwordId;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button newUserButton;
+
+    @FXML
+    private Button forgotPassword;
+
+    @FXML
+    void LoginClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void forgotPasswordClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newUserButtonClick(ActionEvent event) {
+
+    }
+
+}
